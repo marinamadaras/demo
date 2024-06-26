b0e84433373b424d3bbac8b192aa40cd3a65f0a4
Marina Mădăraș <m.madara@student.tudelft.nl>
2024-06-26 12:14:53 +0200

    Adds test classes

---
+++ project/src/test/java/com/demo/model/ProductTest.java
@@ -0,0 +1,42 @@
+package com.demo.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getProductId() {
+    }
+
+    @Test
+    void setProductId() {
+    }
+
+    @Test
+    void getProductName() {
+    }
+
+    @Test
+    void setProductName() {
+    }
+
+    @Test
+    void getProductPrice() {
+    }
+
+    @Test
+    void setProductPrice() {
+    }
+}
---
+++ project/src/test/java/com/demo/model/UserTest.java
@@ -0,0 +1,42 @@
+package com.demo.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getUserId() {
+    }
+
+    @Test
+    void setUserId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+}
