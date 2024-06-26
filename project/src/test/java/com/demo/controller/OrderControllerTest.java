ac610ab62934b49ad129ff46a2ede16aff462ee9
Marina Mădăraș <m.madara@student.tudelft.nl>
2024-06-26 12:17:47 +0200

    Adds order tests

---
+++ project/src/test/java/com/demo/controller/OrderControllerTest.java
@@ -0,0 +1,26 @@
+package com.demo.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void orderForm() {
+    }
+
+    @Test
+    void saveOrder() {
+    }
+}
---
+++ project/src/test/java/com/demo/model/OrderTest.java
@@ -0,0 +1,50 @@
+package com.demo.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getOrderId() {
+    }
+
+    @Test
+    void setOrderId() {
+    }
+
+    @Test
+    void getProduct() {
+    }
+
+    @Test
+    void setProduct() {
+    }
+
+    @Test
+    void getQuantity() {
+    }
+
+    @Test
+    void setQuantity() {
+    }
+
+    @Test
+    void getTotalPrice() {
+    }
+
+    @Test
+    void setTotalPrice() {
+    }
+}
